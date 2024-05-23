657a621c0475340e0e3f5b30dfbe392365cdaa6b
wanderson648 <testepedroca@gmail.com>
2024-05-23 12:24:14 -0300

    cria controller de Candidate

---
+++ src/main/java/com/wo/modules/candidate/CandidateEntity.java
@@ -0,0 +1,19 @@
+package com.wo.modules.candidate;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
--- src/main/java/com/wo/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/wo/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,19 @@
 package com.wo.modules.candidate.controllers;
 
+import com.wo.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
 public class CandidateController {
 
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidate");
+        System.out.println(candidateEntity.getEmail());
+    }
 
 }
