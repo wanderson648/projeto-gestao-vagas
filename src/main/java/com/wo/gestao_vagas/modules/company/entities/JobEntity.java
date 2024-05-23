3dac2a5d863dfa8aed3a742eb051598b5955ba41
wanderson648 <testepedroca@gmail.com>
2024-05-23 14:50:17 -0300

    cria entities Company e Job

---
+++ src/main/java/com/wo/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,31 @@
+package com.wo.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity(name = "job")
+public class JobEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
