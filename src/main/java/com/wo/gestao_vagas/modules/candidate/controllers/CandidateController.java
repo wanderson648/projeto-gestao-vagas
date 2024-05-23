da2e8af363d4a0f8e1919f93cc8a818976ca9693
wanderson648 <testepedroca@gmail.com>
2024-05-23 14:32:20 -0300

    verifica se usuÃario ja existe e cria camada de service

--- src/main/java/com/wo/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/wo/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,12 @@
 package com.wo.gestao_vagas.modules.candidate.controllers;
 
+import com.wo.gestao_vagas.exceptions.UserFoundException;
 import com.wo.gestao_vagas.modules.candidate.CandidateEntity;
+import com.wo.gestao_vagas.modules.candidate.CandidateRepository;
+import com.wo.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
+@RequiredArgsConstructor
 public class CandidateController {
 
+    private final CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidate");
-        System.out.println(candidateEntity.getEmail());
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/com/wo/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,25 @@
+package com.wo.gestao_vagas.modules.candidate.useCases;
+
+import com.wo.gestao_vagas.exceptions.UserFoundException;
+import com.wo.gestao_vagas.modules.candidate.CandidateEntity;
+import com.wo.gestao_vagas.modules.candidate.CandidateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateCandidateUseCase {
+
+    private final CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        candidateRepository.findByUsernameOrEmail(
+                        candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        return candidateRepository.save(candidateEntity);
+    }
+
+}
