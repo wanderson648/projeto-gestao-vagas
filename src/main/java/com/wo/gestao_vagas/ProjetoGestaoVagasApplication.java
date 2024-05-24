8547ad9a1a6d59ca8f535a6af74c3280887d0357
wanderson648 <testepedroca@gmail.com>
2024-05-24 19:07:52 -0300

    adiciona swagger-doc

--- src/main/java/com/wo/gestao_vagas/ProjetoGestaoVagasApplication.java
+++ src/main/java/com/wo/gestao_vagas/ProjetoGestaoVagasApplication.java
@@ -1,13 +1,18 @@
 package com.wo.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "Gestão de Vagas", description = "API responsável pela gestão de vagas", version = "1")
+)
 public class ProjetoGestaoVagasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoGestaoVagasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoGestaoVagasApplication.class, args);
+    }
 
 }
